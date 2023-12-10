b2dd486679228c9c3c785343bb588afad15b41eb
ArmaniCodes <armanibep@gmail.com>
2023-12-10 18:26:47 -0500

    Setup HangManGame Class

---
+++ src/main/java/_HangMan/HangMan.java
@@ -0,0 +1,18 @@
+package _HangMan;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class HangMan {
+	private int length;
+	private String word;
+	private HashMap<Character,ArrayList<Integer>> hp = new HashMap<>();
+	private HashSet<Character> hs = new HashSet<>();
+	private int discoveredLetters = 0;
+	
+	
+	
+	
+	
+}
