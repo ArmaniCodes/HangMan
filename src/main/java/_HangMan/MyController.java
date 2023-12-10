b5a9e993da3e5e2b289851c93ebef327cf7a63eb
ArmaniCodes <armanibep@gmail.com>
2023-12-10 18:08:19 -0500

    Add method to change stickMan Image

--- src/main/java/_HangMan/MyController.java
+++ src/main/java/_HangMan/MyController.java
@@ -1,5 +1,23 @@
 package _HangMan;
 
-public class MyController {
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+public class MyController {
+	@FXML
+	private ImageView myImageView;
+	
+	private int currentImageNum = 0;
+	private String imageDirectory = "/HangManImages/";
+	
+	
+	
+	private void changeImage() {
+		if (currentImageNum >= 7) {
+			currentImageNum = 0;
+		}
+		Image newImage = new Image(getClass().getResourceAsStream(imageDirectory + ++currentImageNum + ".png" ));
+		myImageView.setImage(newImage);
+	}
 }
