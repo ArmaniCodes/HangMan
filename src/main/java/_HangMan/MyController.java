4c826ac730827bbfa5deab2616bbb803a25e0b84
ArmaniCodes <armanibep@gmail.com>
2023-12-10 18:14:09 -0500

    Add start button functionality to change Image for Testing

--- src/main/java/_HangMan/MyController.java
+++ src/main/java/_HangMan/MyController.java
@@ -1,17 +1,29 @@
 package _HangMan;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class MyController {
 	@FXML
 	private ImageView myImageView;
+	@FXML
+	private Button startButton;
 	
 	private int currentImageNum = 0;
-	private String imageDirectory = "/HangManImages/";
+	private String imageDirectory = "/_HangMan/HangManImages/";
 	
+
+	public void initialize() {
+		startButton.setOnAction(event -> {
+			changeImage();
+			
+		}
+);
+		
 	
+	}
 	
 	private void changeImage() {
 		if (currentImageNum >= 7) {
