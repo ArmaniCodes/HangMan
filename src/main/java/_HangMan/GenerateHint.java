3f70cae072cba7ab9b2b3cf0f9c5d359ac7f7fae
ArmaniCodes <armanibep@gmail.com>
2023-12-11 00:16:34 -0500

    Add method to parse the JSON and return the definition of the word

--- src/main/java/_HangMan/GenerateHint.java
+++ src/main/java/_HangMan/GenerateHint.java
@@ -4,6 +4,24 @@ public class GenerateHint extends GenerateWord {
 
 	private static String URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 	
+	public static String parseDefinition(String json) {
+		
+		   int defIndex = json.indexOf("\"definition\":\"");
+		   if (defIndex != -1){
+			   defIndex += "\"definition\":\"".length();
+			   int defEndIndex = json.indexOf("\"", defIndex);
+			   
+			   if (defEndIndex != -1) {
+	                // Extract the definition
+	                String definition = json.substring(defIndex, defEndIndex);
+
+	               return definition;
+	            }
+		   }
+		return null;
+	}
+	
+	
 	public static String generateDefinition(String word)  {
 		String response = "";
 		try {
@@ -13,7 +31,7 @@ public class GenerateHint extends GenerateWord {
 	        e.printStackTrace();
 	    }
 		
-		return response;
+		return parseDefinition(response);
 	}
 	
 }
