efd7cbb3ccbea23041896606c3a1434281dd4fb6
ArmaniCodes <armanibep@gmail.com>
2023-12-11 00:07:41 -0500

    Add try and catch in case exception

--- src/main/java/_HangMan/GenerateHint.java
+++ src/main/java/_HangMan/GenerateHint.java
@@ -4,11 +4,14 @@ public class GenerateHint extends GenerateWord {
 
 	private static String URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 	
-	public static String generateDefinition(String word) throws Exception {
-		
-		String response = sendGetRequest(URL+word);
-		
-		
+	public static String generateDefinition(String word)  {
+		String response = "";
+		try {
+		response = sendGetRequest(URL+word);
+		}
+		catch (Exception e) {
+	        e.printStackTrace();
+	    }
 		
 		return response;
 	}
