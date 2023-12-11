bea8a0f316458f29443fd7209b6168bc153f4335
ArmaniCodes <armanibep@gmail.com>
2023-12-11 00:05:48 -0500

    Add URL for definition API

--- src/main/java/_HangMan/GenerateHint.java
+++ src/main/java/_HangMan/GenerateHint.java
@@ -1,5 +1,16 @@
 package _HangMan;
 
-public class GenerateHint {
+public class GenerateHint extends GenerateWord {
 
+	private static String URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+	
+	public static String generateDefinition(String word) throws Exception {
+		
+		String response = sendGetRequest(URL+word);
+		
+		
+		
+		return response;
+	}
+	
 }
