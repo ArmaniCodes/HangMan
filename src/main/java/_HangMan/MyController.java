e2387e03dee65ed8db53ae142632419cae349fca
ArmaniCodes <armanibep@gmail.com>
2023-12-10 22:37:54 -0500

    Add method to check user inputed letter & replace underline if right.

--- src/main/java/_HangMan/MyController.java
+++ src/main/java/_HangMan/MyController.java
@@ -1,5 +1,7 @@
 package _HangMan;
 
+import java.util.ArrayList;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -25,132 +27,149 @@ public class MyController {
 	private TextField submitField;
 	@FXML
 	private Button submitButton;
-	
+
 	private Text[] underScoreList;
-	private int currentImageNum = 0;
+	private int currentImageNum = 1;
 	private String imageDirectory = "/_HangMan/HangManImages/";
-	
-	//Game of hangMan
+
+	// Game of hangMan
 	private HangMan hm;
-	
+
 	private void enableSubmits() {
 		submitField.setDisable(false);
 		submitField.setVisible(true);
 		submitButton.setDisable(false);
 		submitButton.setVisible(true);
-		
+
 	}
-	
+
 	private void disableSubmits() {
 		submitField.setDisable(true);
 		submitField.setVisible(false);
 		submitButton.setDisable(true);
 		submitButton.setVisible(false);
-		
+
 	}
-		
+
 	private void disableStartButton() {
 		startButton.setDisable(true);
 		startButton.setVisible(false);
 	}
-	
+
 	private void enableStartButton() {
 		startButton.setDisable(false);
 		startButton.setVisible(true);
 	}
-	
+
 	// Ensure user puts a numerical character in range
 	private void inputValidation() {
 		String length = lengthBox.getText();
 		int lengthOfWord = -1;
-		try { 
+		try {
 			lengthOfWord = Integer.parseInt(length);
-		}catch (NumberFormatException e) {
+		} catch (NumberFormatException e) {
 			System.out.println("Invalid input. Please enter a valid number.");
 			return;
 		}
-		if (  lengthOfWord <= 15 && lengthOfWord > 1) {
+		if (lengthOfWord <= 15 && lengthOfWord > 1) {
 			hm = new HangMan(lengthOfWord);
-		}
-		else {
+		} else {
 			System.out.println("Invalid Range. Range 2-15");
 		}
-		
 	}
-	
-	
-	//Formats each new underLine and sets their position to the previous underlines position + 25 
+
+	// Formats each new underLine and sets their position to the previous underlines
+	// position + 25
 	private void formatUnderLine(Text punderline, Text underline) {
-		underline.setFont(Font.font("monospace" , 35.0));
+		underline.setFont(Font.font("monospace", 35.0));
 		underline.setLayoutX((punderline).getLayoutX() + 25);
 		underline.setLayoutY(345);
 		this.myAnchor.getChildren().add(underline);
 	}
-	
+
 	private void setUpWords() {
-		this.underScoreList = new Text[this.hm.getWordLength()];	// Initialize underScore Array
-		
+		this.underScoreList = new Text[this.hm.getWordLength()]; // Initialize underScore Array
+
 		// Initialize First underline
 		Text ogUnderLine = new Text("_");
-		ogUnderLine.setFont(Font.font("monospace" , 35.0));
+		ogUnderLine.setFont(Font.font("monospace", 35.0));
 		ogUnderLine.setLayoutX(14);
 		ogUnderLine.setLayoutY(345);
 		this.myAnchor.getChildren().add(ogUnderLine);
 		underScoreList[0] = ogUnderLine;
-		
-		//Create other underLines
-		for (int i = 1; i < this.hm.getWordLength(); ++i ) {
+
+		// Create other underLines
+		for (int i = 1; i < this.hm.getWordLength(); ++i) {
 			Text underscoreText = new Text("_");
-			formatUnderLine(underScoreList[i-1], underscoreText);
+			formatUnderLine(underScoreList[i - 1], underscoreText);
 			this.underScoreList[i] = underscoreText;
 		}
 	}
 
 	private void startGame() {
-		//If inputLength checkbox Checked then we check if user input is Correct
+		// If inputLength checkbox Checked then we check if user input is Correct
 		if (this.myCheckBox.isSelected()) {
 			inputValidation();
 			setUpWords();
-		}
-		else {
+		} else {
 			hm = new HangMan();
 			setUpWords();
 		}
-		
+
 		// If hangman Game created then we can continue with game setup
-		if (hm != null){
+		if (hm != null) {
 			disableStartButton();
 			enableSubmits();
+			changeImage();
 		}
-		
-		
-		
 	}
 	
 	
 	
+	
+	
+	private void submitAnswer() {
+
+		String letter = submitField.getText();
+		if (letter.length() == 1 && Character.isAlphabetic(letter.charAt(0))) { // Check if valid letter
+			ArrayList<Integer> indexlist = hm.checkLetter(letter.charAt(0));
+			char lttr = letter.charAt(0);
+			if (indexlist == null) {
+				changeImage(); // if returns null then we know letter doesn't exist in word so we can change
+								// image of stickman!
+			} else {
+
+				if (!(hm.isDiscovered(lttr))) { // If returns false then letter hasn't been discovered yet
+					for (int i : indexlist) {   // Loop through indexlist and discover letter as well as change underline to the letter
+						hm.discoverLetter(lttr);
+						underScoreList[i].setText(letter);
+					}
+
+				}
+
+			}
+
+		}
+
+	}
+
 	public void initialize() {
 		startButton.setOnAction(event -> {
 			startGame();
-		}
-);
-	
-		submitButton.setOnAction(event ->{
-			
-			
-			
 			
 		});
-		
-		
-		
+
+		submitButton.setOnAction(event -> {
+			submitAnswer();
+		});
+
 	}
-	
+
 	private void changeImage() {
 		if (currentImageNum >= 7) {
 			currentImageNum = 0;
 		}
-		Image newImage = new Image(getClass().getResourceAsStream(imageDirectory + ++currentImageNum + ".png" ));
+		Image newImage = new Image(getClass().getResourceAsStream(imageDirectory + ++currentImageNum + ".png"));
 		myImageView.setImage(newImage);
 	}
 }
