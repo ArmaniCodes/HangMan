096d8b602be92d0df7c8a4080c33f769407ebdd3
ArmaniCodes <armanibep@gmail.com>
2023-12-11 20:13:45 -0500

    Fix word definition parsing

--- src/main/java/_HangMan/GenerateHint.java
+++ src/main/java/_HangMan/GenerateHint.java
@@ -7,7 +7,21 @@ public class GenerateHint extends GenerateWord {
 	private static String URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 	
 	public static String parseDefinition(String json) {
-		return null;
+		JSONArray jsonArray = new JSONArray(json);
+		JSONObject firstObj = jsonArray.getJSONObject(0);
+		JSONArray meaningsArray = firstObj.getJSONArray("meanings");
+		
+		if (meaningsArray.length() > 0) {
+			JSONObject firstMeaning = meaningsArray.getJSONObject(0);
+			JSONArray definitionsArray = firstMeaning.getJSONArray("definitions");
+			  if (definitionsArray.length() > 0) {
+				  JSONObject firstDefinition = definitionsArray.getJSONObject(0);
+				  String definition = firstDefinition.getString("definition");
+				  return definition;
+			  }
+		}
+		
+		return "Sorry! No Hint";
 	}
 	
 	
